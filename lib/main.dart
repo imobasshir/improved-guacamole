3158d94ed21e16d5fd698080cb9052ee42d59917
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-01-14 19:50:13 +0530

    done of container

--- lib/main.dart
+++ lib/main.dart
@@ -12,17 +12,37 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(
-            decoration: BoxDecoration(
-              border: Border.all(
-                width: 6.0,
-                color: Colors.grey,
-                style: BorderStyle.solid,
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 6.0,
+                      color: Colors.grey,
+                      style: BorderStyle.solid,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(3.5)),
+                  ),
+                  margin: EdgeInsets.all(8.0),
+                  child: Image.asset('images/flutter.png'),
+                ),
               ),
-              borderRadius: BorderRadius.all(Radius.circular(3.5)),
-            ),
-            margin: EdgeInsets.all(8.0),
-            child: Image.asset('images/flutter.png'),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 6.0,
+                      color: Colors.grey,
+                      style: BorderStyle.solid,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(3.5)),
+                  ),
+                  margin: EdgeInsets.all(8.0),
+                  child: Image.asset('images/flutter.png'),
+                ),
+              ),
+            ],
           ),
         ),
       ),
