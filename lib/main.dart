a3d163801bf94cca6a53791ee9c7f4c1c925a441
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-01-16 20:20:56 +0530

    fixes

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,6 @@ class MyApp extends StatelessWidget {
                         ),
                       ),
                     ),
-                    Divider(),
                   ],
                 ),
               ),
