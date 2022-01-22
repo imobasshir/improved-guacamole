92f0a723da0e2da0800937c38e3a9a22dd47d1a3
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-01-22 23:57:21 +0530

    textfield intro

--- lib/main.dart
+++ lib/main.dart
@@ -12,137 +12,42 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: ListView(
+          child: Column(
             children: [
               Image.asset('images/learn_hero.png'),
-              const Divider(),
-              Padding(
-                padding: const EdgeInsets.all(32.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Column(
-                        children: [
-                          Row(
-                            children: const [
-                              Text(
-                                'Learning Flutter',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18,
-                                  fontStyle: FontStyle.italic,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: const [
-                              Text(
-                                'Learning Flutter by learning hero',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w300,
-                                  fontSize: 16,
-                                  fontStyle: FontStyle.italic,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                    const Icon(
-                      Icons.star_rate,
-                      color: Colors.red,
-                    ),
-                    const Text(
-                      '5',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontStyle: FontStyle.italic,
-                      ),
-                    ),
-                  ],
+              const SizedBox(
+                height: 20.0,
+              ),
+              const Text(
+                'TextField Intro',
+                style: TextStyle(
+                  color: Colors.lightBlue,
+                  fontSize: 22,
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Column(
-                        children: const [
-                          Icon(
-                            Icons.phone,
-                            color: Colors.blue,
-                          ),
-                          Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Text(
-                              'Phone',
-                              style: TextStyle(
-                                color: Colors.blue,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Expanded(
-                      child: Column(
-                        children: const [
-                          Icon(
-                            Icons.directions,
-                            color: Colors.blue,
-                          ),
-                          Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Text(
-                              'Route',
-                              style: TextStyle(
-                                color: Colors.blue,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Expanded(
-                      child: Column(
-                        children: const [
-                          Icon(
-                            Icons.share,
-                            color: Colors.blue,
-                          ),
-                          Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Text(
-                              'Share',
-                              style: TextStyle(
-                                color: Colors.blue,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+              const SizedBox(
+                height: 20.0,
+              ),
+              const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    label: Text('Name'),
+                    hintText: 'Enter Name',
+                  ),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Row(
-                  children: const [
-                    Expanded(
-                      child: Text(
-                        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-                        'Alps. Situated 1,578 meters above sea level, it is one of the '
-                        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-                        'half-hour walk through pastures and pine forest, leads you to the '
-                        'lake, which warms to 20 degrees Celsius in the summer. Activities '
-                        'enjoyed here include rowing, and riding the summer toboggan run.',
-                      ),
-                    ),
-                  ],
+              const SizedBox(
+                height: 20.0,
+              ),
+              const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    label: Text('Password'),
+                    hintText: 'Enter Password',
+                  ),
+                  obscureText: true,
                 ),
               ),
             ],
