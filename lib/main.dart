a79bada96a39a474a3294beb9d4806d8942db87e
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-02-02 19:43:25 +0530

    clean project

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_test_application_1/calc/calculator.dart';
-import 'package:flutter_test_application_1/pages/chats.dart';
-import 'package:flutter_test_application_1/pages/demo.dart';
-import 'package:flutter_test_application_1/pages/form.dart';
-import 'package:flutter_test_application_1/pages/login_page.dart';
-import 'package:flutter_test_application_1/pages/next_page.dart';
-import 'package:flutter_test_application_1/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -26,14 +19,14 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
-      routes: {
-        "/": (context) => const MyCalculator(),
-        MyRoutes.home: (context) => const HomePage(),
-        MyRoutes.loginRoute: (context) => const LoginPage(),
-        MyRoutes.demo: (context) => const Demo(),
-        "/form": (context) => const FormView(),
-        MyRoutes.calc: (context) => const MyCalculator(),
-      },
+      // routes: {
+      //   "/": (context) => const MyCalculator(),
+      //   MyRoutes.home: (context) => const HomePage(),
+      //   MyRoutes.loginRoute: (context) => const LoginPage(),
+      //   MyRoutes.demo: (context) => const Demo(),
+      //   "/form": (context) => const FormView(),
+      //   MyRoutes.calc: (context) => const MyCalculator(),
+      // },
     );
   }
 }
--- lib/utils/routes.dart
+++
@@ -1,8 +0,0 @@
-class MyRoutes {
-  static String loginRoute = "/login";
-  static String home = "/home";
-  static String chat = "/chat";
-  static String demo = "/demo";
-  static String form = "/form";
-  static String calc = "/calc";
-}
