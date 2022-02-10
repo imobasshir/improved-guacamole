2bf18bdf3ad32c010d7d9a7ad3ed0cd5836caf12
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-02-11 02:19:45 +0530

    done

--- lib/save_data/main.dart
+++ lib/save_data/main.dart
@@ -10,7 +10,10 @@ class SaveData extends StatefulWidget {
 
 class _SaveDataState extends State<SaveData> {
   final _usernameController = TextEditingController();
-  Gender _selectedGender = Gender.MALE;
+  var _selectedGender = Gender.MALE;
+  var _selectedLanguage = Set<ProgrammingLanguage>();
+  var _isEmployed = false;
+  bool isCheck = false;
 
   @override
   Widget build(BuildContext context) {
@@ -30,29 +33,90 @@ class _SaveDataState extends State<SaveData> {
             ),
           ),
           RadioListTile(
-            title: const Text('Male'),
-            value: Gender.MALE,
-            groupValue: _selectedGender,
-            onChanged: (newValue) => setState(() => _selectedGender = newValue),
-          ),
+              title: const Text('Male'),
+              value: Gender.MALE,
+              groupValue: _selectedGender,
+              onChanged: (Gender? newValue) {
+                setState(() {
+                  _selectedGender = newValue!;
+                });
+              }),
           RadioListTile(
-            title: const Text('Female'),
-            value: Gender.FEMALE,
-            groupValue: _selectedGender,
-            onChanged: (newValue) => setState(() => _selectedGender = newValue),
-          ),
+              title: const Text('Female'),
+              value: Gender.FEMALE,
+              groupValue: _selectedGender,
+              onChanged: (Gender? newValue) {
+                setState(() {
+                  _selectedGender = newValue!;
+                });
+              }),
           RadioListTile(
             title: const Text('Others'),
             value: Gender.OTHERS,
             groupValue: _selectedGender,
-            onChanged: (newValue) {
+            onChanged: (Gender? newValue) {
               setState(() {
-                _selectedGender = newValue;
+                _selectedGender = newValue!;
               });
             },
+          ),
+          const Divider(),
+          CheckboxListTile(
+            title: const Text('Java'),
+            value: _selectedLanguage.contains(ProgrammingLanguage.JAVA),
+            onChanged: (_) {
+              _selectedLanguage.contains(ProgrammingLanguage.JAVA)
+                  ? _selectedLanguage.remove(ProgrammingLanguage.JAVA)
+                  : _selectedLanguage.add(ProgrammingLanguage.JAVA);
+            },
+          ),
+          CheckboxListTile(
+            title: const Text('Dart'),
+            value: _selectedLanguage.contains(ProgrammingLanguage.DART),
+            onChanged: (_) {
+              _selectedLanguage.contains(ProgrammingLanguage.DART)
+                  ? _selectedLanguage.remove(ProgrammingLanguage.DART)
+                  : _selectedLanguage.add(ProgrammingLanguage.DART);
+            },
+          ),
+          CheckboxListTile(
+            title: const Text('Cpp'),
+            value: _selectedLanguage.contains(ProgrammingLanguage.CPP),
+            onChanged: (_) {
+              _selectedLanguage.contains(ProgrammingLanguage.CPP)
+                  ? _selectedLanguage.remove(ProgrammingLanguage.CPP)
+                  : _selectedLanguage.add(ProgrammingLanguage.CPP);
+            },
+          ),
+          StatefulBuilder(
+            builder: (context, _setState) => CheckboxListTile(
+              title: const Text('Java Script'),
+              value: _selectedLanguage.contains(ProgrammingLanguage.JS),
+              onChanged: (_) {
+                _selectedLanguage.contains(ProgrammingLanguage.JS)
+                    ? _selectedLanguage.remove(ProgrammingLanguage.JS)
+                    : _selectedLanguage.add(ProgrammingLanguage.JS);
+              },
+            ),
+          ),
+          SwitchListTile(
+              title: const Text('Is Employed'),
+              value: _isEmployed,
+              onChanged: (newValue) => setState(() => _isEmployed = newValue)),
+          TextButton(
+            onPressed: _saveSettings,
+            child: const Text('Save Settings'),
           ),
         ],
       ),
     );
+  }
+
+  void _saveSettings() {
+    final newSettings = Settings(
+        username: _usernameController.text,
+        gender: _selectedGender,
+        programmingLanguages: _selectedLanguage,
+        isEmployed: _isEmployed);
   }
 }
--- lib/save_data/models.dart
+++ lib/save_data/models.dart
@@ -1,1 +1,17 @@
 enum Gender { MALE, FEMALE, OTHERS }
+
+enum ProgrammingLanguage { JAVA, DART, CPP, JS }
+
+class Settings {
+  final String username;
+  final Gender gender;
+  final Set<ProgrammingLanguage> programmingLanguages;
+  final bool isEmployed;
+
+  Settings({
+    required this.username,
+    required this.gender,
+    required this.programmingLanguages,
+    required this.isEmployed,
+  });
+}
